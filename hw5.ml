5bcb8f753cc45e67e5ffbcd4a18e7812f42886ea
Emile Marty <34513785+emilemarty@users.noreply.github.com>
2020-10-29 12:25:37 -0400

    Add files via upload

--- hw5.ml
+++ hw5.ml
@@ -14,7 +14,7 @@ type 'a graph = {
 
 (* Reminder: If a test case requires multiple arguments, use a tuple:
 let myfn_with_2_args_tests = [
-  ((arg1, arg1), (expected_output))
+((arg1, arg1), (expected_output))
 ]
 *)
 
@@ -29,18 +29,18 @@ let neighbours (g: 'a graph) (vertex: 'a) : ('a * weight) list =
 
 (* TODO: Implement find_path. *)
 let find_path (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
-  let rec aux_node node visited =
+  let rec aux_node (node: 'a * weight) (visited : 'a list) : ('a list * weight) =
     raise NotImplemented
-  and aux_list nodes visited =
+  and aux_list (nodes: ('a * weight) list) (visited: 'a list) : ('a list * weight) =
     raise NotImplemented
   in
   raise NotImplemented
 
 (* TODO: Implement find_path'. *)
 let find_path' (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
-  let rec aux_node node visited fc sc =
+  let rec aux_node (node: 'a * weight) (visited : 'a list) fc sc : ('a list * weight)=
     raise NotImplemented
-  and aux_list nodes visited fc sc =
+  and aux_list (nodes: ('a * weight) list) (visited: 'a list) fc sc : ('a list * weight) =
     raise NotImplemented
   in
   raise NotImplemented
@@ -48,9 +48,9 @@ let find_path' (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
 
 (* TODO: Implement find_all_paths *)
 let find_all_paths (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) list = 
-  let rec aux_node node visited =
+  let rec aux_node (node: 'a * weight) (visited : 'a list) : ('a list * weight) list =
     raise NotImplemented  
-  and aux_list nodes visited = 
+  and aux_list (nodes: ('a * weight) list) (visited: 'a list) : ('a list * weight) list =
     raise NotImplemented  
   in
   raise NotImplemented  
