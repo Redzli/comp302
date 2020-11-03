b672e3666463aed63b7752e1d6c2b185eb40661d
Emile Marty <34513785+emilemarty@users.noreply.github.com>
2020-11-03 18:42:18 -0500

    Add files via upload

--- hw6.ml
+++ hw6.ml
@@ -58,8 +58,14 @@ let eval e = eval' (parse e)
 
 (* ---------- Hamming Numbers ----------- *)
 
-let rec merge s1 s2 = 
-  ()
+let rec merge (s1: 'a str) (s2: 'a str): 'a str = {
+  hd = if (s1.hd < s2.hd) then s1.hd else s2.hd;
+  tl = if (s1.hd < s2.hd) then delay (fun () -> merge (force s1.tl) s2)
+  else if (s1.hd > s2.hd) then delay (fun () -> merge s1 (force s2.tl))
+  else delay (fun () -> merge (force s1.tl) (force s2.tl));
+}
 
-let rec hamming_series = 
-  ()
+let rec hamming_series: 'a str = {
+  hd = 1;
+  tl = Susp (fun () -> merge (times 5 hamming_series) (merge (times 2 hamming_series) (times 3 hamming_series)));
+}
