1d7117af44298da0f1cdc0d1cad4c24743cab2d2
Emile Marty <34513785+emilemarty@users.noreply.github.com>
2020-11-03 14:15:38 -0500

    Add files via upload

--- hw6.ml
+++ hw6.ml
@@ -22,13 +22,43 @@ let rec parseExp (toklist: token list) (sc: (token list -> exp -> 'a)) : 'a =
        | _ -> raise (Error "Expected a single semicolon"))
 
 and parseSExp (toklist: token list) (sc: (token list -> exp -> 'a)) : 'a =
-  raise NotImplemented
+  parsePExp
+    toklist
+    (fun toklist' exp -> match toklist' with
+      | PLUS :: toklist'' -> parseSExp toklist'' (fun toklist''' exp' -> sc toklist''' (Sum(exp,exp')))
+      | SUB :: toklist'' -> parseSExp toklist'' (fun toklist''' exp' -> sc toklist''' (Minus(exp,exp')))
+      | _ -> sc toklist' exp)
 
 and parsePExp (toklist: token list) (sc: (token list -> exp -> 'a)) : 'a =
-  raise NotImplemented
+  parseAtom
+    toklist
+    (fun toklist' exp -> match toklist' with
+      | TIMES :: toklist'' -> parsePExp toklist'' (fun toklist''' exp' -> sc toklist''' (Prod(exp,exp')))
+      | DIV :: toklist'' -> parsePExp toklist'' (fun toklist''' exp' -> sc toklist''' (Div(exp,exp')))
+      | _ -> sc toklist' exp)
 
 and parseAtom (toklist: token list) (sc: (token list -> exp -> 'a)) : 'a =
-  raise NotImplemented
+  match toklist with
+  | (INT n) :: toklist' -> sc toklist' (Int n)
+  | LPAREN :: toklist' -> parseSExp toklist' (fun toklist'' exp -> match toklist'' with 
+      | RPAREN :: t -> sc t exp
+      | _ -> raise (Error "Expected a closing parenthesis"))
+  | _ -> raise (Error "List must begin with INT or LPAREN")
+
+
+
+parseAtom ([INT 3]) (fun toklist exp -> exp)
+
+let rec parseAtom (toklist: token list) (sc: (token list -> exp -> 'a)) : 'a =
+  match toklist with
+  | INT :: toklist'' -> sc toklist'' (Int)
+  |
+  
+  parseAtom
+    toklist
+    (fun toklist' exp -> match toklist' with
+    | INT :: toklist'' -> sc toklist'' (Int)
+    | [] -> exp)
 
 (* parse : string -> exp *)
 let parse string =
@@ -43,8 +73,8 @@ let eval e = eval' (parse e)
 
 (* ---------- Hamming Numbers ----------- *)
 
-let rec merge (s1: 'a str) (s2: 'a str) : 'a str = 
-  raise NotImplemented
+let rec merge s1 s2 = 
+  ()
 
 let rec hamming_series = 
-  raise NotImplemented
+  ()
