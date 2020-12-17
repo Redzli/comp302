12db9dd0292b388ae8e9024668ac6082b304497d
Emile Marty <34513785+emilemarty@users.noreply.github.com>
2020-12-17 19:08:34 +0100

    Add files via upload

--- finalProject.ml
+++ finalProject.ml
@@ -16,7 +16,7 @@ let parse_tests : (string * (string, exp) either) list = [
 ]
 
 
-let parse_toExp (program: string) : exp =
+let toExp (program: string) : exp =
   match P.parse program with
   | Right e -> e
   | Left e -> stuck "Invalid Program"
@@ -38,25 +38,26 @@ let rec flatExp (input: exp) : exp =
 
 let free_vars_tests : (exp * name list) list = [
   (Int 10, []);
-  (parse_toExp valid_program_1, []);
-  (parse_toExp valid_program_2, []);
-  (parse_toExp valid_program_3, []);
-  (parse_toExp valid_program_4, []);
-  (parse_toExp valid_program_5, []);
-  (parse_toExp valid_program_6, []);
-  (parse_toExp valid_program_7, []);
-  (parse_toExp valid_program_8, []);
-  (parse_toExp valid_program_9, []);
-  (parse_toExp valid_program_10, []);
+  (toExp valid_program_1, []);
+  (toExp valid_program_2, []);
+  (toExp valid_program_3, []);
+  (toExp valid_program_4, []);
+  (toExp valid_program_5, []);
+  (toExp valid_program_6, []);
+  (toExp valid_program_7, []);
+  (toExp valid_program_8, []);
+  (toExp valid_program_9, []);
+  (toExp valid_program_10, []);
   
-  (parse_toExp "fn x : int => x * x;", []);
-  (parse_toExp "fn x : int => x * y;", ["y"]); 
-  (parse_toExp "fn x : int => x * y * z;", ["y"; "z"]);
-  (parse_toExp "let fun f : int = fn x : int => x * y * z in 6 end;", ["y"; "z"]);
-  (parse_toExp "let fun f : int = fn x : int => x * y * z in apply 6 end;", ["y"; "z"; "apply"]);
-  (parse_toExp "let val x = 3 in let val (y1,y2) = (x, 2) in x + y1 end end;", []);
-  (parse_toExp "let val x = 3\n val (y1,y2) = (x, 2) in x + y1 end;", []);
-  (parse_toExp "let val x = x name y = x + 3 val z = 3 in x + y + z end;", ["x"]);
+  (toExp "fn x : int => x * x;", []);
+  (toExp "fn x : int => x * y;", ["y"]); 
+  (toExp "fn x : int => x * y * z;", ["y"; "z"]);
+  (toExp "let fun f : int = fn x : int => x * y * z in 6 end;", ["y"; "z"]);
+  (toExp "let fun f : int = fn x : int => x * y * z in apply 6 end;", ["y"; "z"; "apply"]);
+  (toExp "let val x = 3 in let val (y1,y2) = (x, 2) in x + y1 end end;", []);
+  (toExp "let val x = 3\n val (y1,y2) = (x, 2) in x + y1 end;", []);
+  (toExp "let val x = 5 name y = x + 3 val z = 3 in x + y + z end;", []);
+  (toExp "let val x = x name y = x + 3 val z = 3 in x + y + z end;", ["x"]);
 ]
 
 (* Q1  : Find the free variables in an expression *)
@@ -90,27 +91,27 @@ let rec free_vars (e : exp) : name list =
 
 let unused_vars_tests : (exp * name list) list = [
   (Int 10, []);
-  (parse_toExp valid_program_1, []);
-  (parse_toExp valid_program_2, []);
-  (parse_toExp valid_program_3, []);
-  (parse_toExp valid_program_4, []);
-  (parse_toExp valid_program_5, []);
-  (parse_toExp valid_program_6, ["x"]);
-  (parse_toExp valid_program_7, []);
-  (parse_toExp valid_program_8, []);
-  (parse_toExp valid_program_9, []);
-  (parse_toExp valid_program_10, ["y"]);
+  (toExp valid_program_1, []);
+  (toExp valid_program_2, []);
+  (toExp valid_program_3, []);
+  (toExp valid_program_4, []);
+  (toExp valid_program_5, []);
+  (toExp valid_program_6, ["x"]);
+  (toExp valid_program_7, []);
+  (toExp valid_program_8, []);
+  (toExp valid_program_9, []);
+  (toExp valid_program_10, ["y"]);
   
-  (parse_toExp "fn x : int => x * x;", []);
-  (parse_toExp "fn x : int => x * y;", []); 
-  (parse_toExp "let fun f : int = fn x : int => x * y * z in 6 end;", ["f"]);
-  (parse_toExp "let fun f : int = fn x : int => f(y) * z in f 6 end;", ["x"]);
-  (parse_toExp "let fun f : int = fn x : int => x * z in f 6 end;", []); 
-  (parse_toExp "let val x = 3 val y = 4 in x + y end;",[]);
-  (parse_toExp "let val x = let val y = 2 in 3 end val y = 4 in x + y end;", ["y"]);
-  (parse_toExp "let val x = true in let val x = 1 in let val x = 3 in x + 5 end end end;", ["x"]);
-  (parse_toExp "let val x = 3 in let val (y1,y2) = (x, 2) in x + y1 end end;", ["y2"]); 
-  (parse_toExp "let val x = 3\n val (y1,y2) = (x, 2) in x + y1 end;", ["y2"]); 
+  (toExp "fn x : int => x * x;", []);
+  (toExp "fn x : int => x * y;", []); 
+  (toExp "let fun f : int = fn x : int => x * y * z in 6 end;", ["f"]);
+  (toExp "let fun f : int = fn x : int => f(y) * z in f 6 end;", ["x"]);
+  (toExp "let fun f : int = fn x : int => x * z in f 6 end;", []); 
+  (toExp "let val x = 3 val y = 4 in x + y end;",[]);
+  (toExp "let val x = let val y = 2 in 3 end val y = 4 in x + y end;", ["y"]);
+  (toExp "let val x = true in let val x = 1 in let val x = 3 in x + 5 end end end;", ["x"]);
+  (toExp "let val x = 3 in let val (y1,y2) = (x, 2) in x + y1 end end;", ["y2"]); 
+  (toExp "let val x = 3\n val (y1,y2) = (x, 2) in x + y1 end;", ["y2"]); 
 ]
 
 (* Q2  : Check variables are in use *)
@@ -133,30 +134,30 @@ let rec unused_vars (e : exp) : name list =
 let subst_tests : (((exp * name) * exp) * exp) list = [
   (((Int 5, "x"), (If (Bool(true), Var"x", Var"y"))), (If (Bool(true), Int 5, Var"y")));
   (((Var "z", "x"), Var "x"), Var "z");
-  (((Var "z", "x"), parse_toExp "fn x : int => x * x;"), 
-   parse_toExp "fn x : int => x * x;");
-  (((Var "z", "x"), parse_toExp "fn z : int => x * x;"), 
+  (((Var "z", "x"), toExp "fn x : int => x * x;"), 
+   toExp "fn x : int => x * x;");
+  (((Var "z", "x"), toExp "fn z : int => x * x;"), 
    (Fn ("1z", Some TInt, Primop (Times, [Var "z"; Var "z"]))));
-  (((Var "z", "x"), parse_toExp "fn z : int => z * x;"), 
+  (((Var "z", "x"), toExp "fn z : int => z * x;"), 
    (Fn ("1z", Some TInt, Primop (Times, [Var "1z"; Var "z"]))));
   
-  (((Var "z", "x"), parse_toExp "let val x = 6 in x * x end;"), 
-   parse_toExp "let val x = 6 in x * x end;");
-  (((Var "z", "x"), parse_toExp "let val (x,y,z) = (1,x,y) in x * x end;"), 
-   parse_toExp "let val (x,y,z) = (1,z,y) in x * x end;");
-  (((Var "x", "a"), parse_toExp "let val (x,y,z) = (1,a,b) in x * a end;"), 
+  (((Var "z", "x"), toExp "let val x = 6 in x * x end;"), 
+   toExp "let val x = 6 in x * x end;");
+  (((Var "z", "x"), toExp "let val (x,y,z) = (1,x,y) in x * x end;"), 
+   toExp "let val (x,y,z) = (1,z,y) in x * x end;");
+  (((Var "x", "a"), toExp "let val (x,y,z) = (1,a,b) in x * a end;"), 
    Let ([Valtuple (Tuple [Int 1; Var "x"; Var "b"], ["1x"; "y"; "z"])],
         Primop (Times, [Var "1x"; Var "x"])));
-  ((((Primop (Times, [Var"x"; Var"y"])), "a"), parse_toExp "let val (x,y,z) = (1,a,b) in x * a end;"), 
+  ((((Primop (Times, [Var"x"; Var"y"])), "a"), toExp "let val (x,y,z) = (1,a,b) in x * a end;"), 
    Let ([Valtuple (Tuple [Int 1; (Primop (Times, [Var"x"; Var"y"])); Var "b"], ["1x"; "2y"; "z"])],
         Primop (Times, [Var "1x"; (Primop (Times, [Var"x"; Var"y"]))])));
   
-  (((Int 1, "x"), parse_toExp "let val x = 5 name y = x + 3 val z = 3 in x + y + z end;"), 
-   parse_toExp "let val x = 5 in let name y = x + 3 in let val z = 3 in x + y + z end end end;");
-  (((Int 1, "x"), parse_toExp "let val x = x name y = x + 3 val z = 3 in x + y + z end;"), 
-   parse_toExp "let val x = 1 in let name y = x + 3 in let val z = 3 in x + y + z end end end;");
-  (((Int 1, "x"), parse_toExp "let val a = x name b = x + 3 val c = 3 in a + b + x end;"), 
-   parse_toExp "let val a = 1 in let name b = 1 + 3 in let val c = 3 in a + b + 1 end end end;");
+  (((Int 1, "x"), toExp "let val x = 5 name y = x + 3 val z = 3 in x + y + z end;"), 
+   toExp "let val x = 5 in let name y = x + 3 in let val z = 3 in x + y + z end end end;");
+  (((Int 1, "x"), toExp "let val x = x name y = x + 3 val z = 3 in x + y + z end;"), 
+   toExp "let val x = 1 in let name y = x + 3 in let val z = 3 in x + y + z end end end;");
+  (((Int 1, "x"), toExp "let val a = x name b = x + 3 val c = 3 in a + b + x end;"), 
+   toExp "let val a = 1 in let name b = 1 + 3 in let val c = 3 in a + b + 1 end end end;");
 ] 
 
 let changeName ((newV, oldV): name * name) (d: dec) : dec =
@@ -203,16 +204,19 @@ and
 
 
 let eval_tests : (exp * exp) list = [
-  (*(parse_toExp valid_program_1, expected);*)
-  (parse_toExp valid_program_2, Int 133);
-  (parse_toExp valid_program_3, Int 120);
-  (parse_toExp valid_program_4, Int 3);
-  (parse_toExp valid_program_5, Int 6);
-  (parse_toExp valid_program_6, Int 6);
-  (parse_toExp valid_program_7, Int 4);
-  (parse_toExp valid_program_8, Int 900);
-  (parse_toExp valid_program_9, Int 1600);
-  (parse_toExp valid_program_10, Int 10);
+  (toExp valid_program_1, Int 300);
+  (toExp valid_program_2, Int 133);
+  (toExp valid_program_3, Int 120);
+  (toExp valid_program_4, Int 3);
+  (toExp valid_program_5, Int 6);
+  (toExp valid_program_6, Int 6);
+  (toExp valid_program_7, Int 4);
+  (toExp valid_program_8, Int 900);
+  (toExp valid_program_9, Int 1600);
+  (toExp valid_program_10, Int 10);
+
+  (Primop (Or, [Bool false; Primop (Or, [Bool false; Primop (Or, [Bool false; Bool true])])]), Bool true);
+  (Primop (And, [Bool true; Primop (And, [Bool true; Primop (And, [Bool true; Bool false])])]), Bool false);
 ]
 
 let weave (xs: name list) (vs: exp list) : (exp * name) list =
@@ -305,12 +309,120 @@ let rec eval : exp -> exp =
 
 
 let infer_tests : ((context * exp) * typ) list = [
+  ((Ctx [], toExp valid_program_1), TInt);
+  ((Ctx [], toExp valid_program_2), TInt);
+  ((Ctx [], toExp valid_program_3), TInt);
+  ((Ctx [], toExp valid_program_4), TInt);
+  ((Ctx [], toExp valid_program_5), TInt);
+  ((Ctx [], toExp valid_program_6), TInt);
+  ((Ctx [], toExp valid_program_7), TInt);
+  ((Ctx [], toExp valid_program_8), TInt);
+  ((Ctx [], toExp valid_program_9), TInt);
+  ((Ctx [], toExp valid_program_10), TInt);
 ]
+
+let rec typeToString (t: typ) =
+  match t with
+  | TArrow   (a, b)   -> typeToString(a) ^ " -> " ^ typeToString(b)
+  | TProduct (a::b::t)-> typeToString(a) ^ " * " ^ typeToString(TProduct (b::t))
+  | TProduct (b::[])  -> typeToString(b)
+  | TInt              -> "int"
+  | TBool             -> "bool"
+  | _                 -> ""
+
+let rec printContext (ctx: (name * typ) list) : unit =
+  match ctx with
+  | [] -> Printf.printf "\n";
+  | (n, t)::rest -> let t = typeToString t in Printf.printf "%s, %s; " n t; printContext rest
 
 (* Q5  : Type an expression *)
 (* Q7* : Implement the argument type inference
          For this question, move this function below the "unify". *)
-let infer (ctx : context) (e : exp) : typ = raise NotImplemented
+let rec infer (ctx : context) (e : exp) : typ =
+  match e with
+  | Int _  -> TInt
+  | Bool _ -> TBool
+  | Var x  -> ctx_lookup ctx x
+
+  | If (e, e1, e2)  ->
+      let (e1T,e2T) = (infer ctx e1, infer ctx e2) in 
+      if (infer ctx e = TBool) then
+        if (typ_eq e1T e2T) then e1T
+        else type_fail "If statement branches must have the same type"
+      else type_fail "If statement guard must have type bool"
+  | Primop (op, l)  ->
+      begin match (op, l) with
+        | (Equals,       [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TBool else type_fail "Equals: Inputs must have type int"
+        | (NotEquals,    [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TBool else type_fail "NotEquals: Inputs must have type int"
+        | (LessThan,     [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TBool else type_fail "LessThan: Inputs must have type int"
+        | (LessEqual,    [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TBool else type_fail "LessEqual: Inputs must have type int"
+        | (GreaterThan,  [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TBool else type_fail "GreaterThan: Inputs must have type int"
+        | (GreaterEqual, [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TBool else type_fail "GreaterEqual: Inputs must have type int"
+        | (Plus,         [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TInt else type_fail "Plus: Inputs must have type int"
+        | (Minus,        [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TInt else type_fail "Minus: Inputs must have type int"
+        | (Times,        [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TInt else type_fail "Times: Inputs must have type int"
+        | (Div,          [e1; e2])   -> 
+            if (infer ctx e1 = TInt) && (infer ctx e2 = TInt) 
+            then TInt else type_fail "Div: Inputs must have type int"
+        | (And,          [e1; e2])   -> 
+            if (infer ctx e1 = TBool) && (infer ctx e2 = TBool) 
+            then TBool else type_fail "And: Inputs must have type bool"
+        | (Or,           [e1; e2])   -> 
+            if (infer ctx e1 = TBool) && (infer ctx e2 = TBool) 
+            then TBool else type_fail "Or: Inputs must have type bool"
+        | (Negate,       [e])        -> 
+            if (infer ctx e = TInt) 
+            then TInt else type_fail "Negate: Input must have type int"
+        | _ -> type_fail "Incorrect number of arguments for primitive operation"
+      end
+  | Tuple (list)    -> TProduct (List.map (infer ctx) list)
+  | Fn (x, t, e)    ->
+      begin match t with
+      | Some (tau) -> let tau' = infer (extend ctx (x, tau)) e in TArrow (tau, tau')
+      | None -> type_fail "No type annotation given for function"
+      end
+  | Rec (f, t, e)   -> infer (extend ctx (f, t)) e
+  | Apply (e1, e2)  -> let tau = infer ctx e2 in
+      begin match infer ctx e1 with
+      | TArrow (t,t') -> if (t = tau) then t' else type_fail "Function argument is of incorrect type"
+      | _ -> type_fail "Expression is not a function, it cannot be applied"
+      end
+  | Anno (e, t)     -> if (infer ctx e) = t then t else type_fail "Annotation and expression type mismatch"
+  | Let (ds, e)     -> let Ctx (gamma') = inferDecs ctx (Ctx []) ds in infer (extend_list ctx gamma') e
+and inferDecs (gamma: context) (acc: context) (ds: dec list) : context =
+  let inferDec (ctx: context) (d: dec) : context =
+    match d with
+    | Val (e, x)       -> Ctx [x, infer ctx e]
+    | Valtuple (e, xs) -> 
+        begin match (infer ctx e) with
+        | TProduct (ts) -> (try Ctx (List.map2 (fun x t -> (x,t)) xs ts)
+                           with Invalid_argument _ -> type_fail "Tuple length mismatch in Let binding")
+        | _ ->  type_fail "Expression in Let binding must evaluate to a tuple"
+        end
+    | ByName (e, x)    -> Ctx [x, infer ctx e]
+  in 
+  match ds with
+  | dec::rest -> let Ctx (gamma1) = inferDec gamma dec in 
+                 inferDecs (extend_list gamma gamma1) (extend_list acc gamma1) rest
+  | [] -> acc
 
 
 let unify_tests : ((typ * typ) * unit) list = [
@@ -355,20 +467,21 @@ let list_to_string el_to_string l : string =
     l
   |> fun str -> "[" ^ str ^ "]"
 
-let run_test name f ts stringify : unit =
+let run_test name f ts stringify : unit = 
   List.iteri
     begin fun idx (input, expected_output) ->
       try
+        reset_ctr();
         let output = f input in
         if output <> expected_output then
           begin
             print_string (name ^ " test #" ^ string_of_int idx ^ " failed\n");
-            print_string (stringify output ^ " <> " ^ stringify expected_output)
+            print_string (stringify output ^ " <> " ^ stringify expected_output ^ "\n")
           end
       with
       | exn ->
           print_string (name ^ " test #" ^ string_of_int idx ^ " raised an exception:\n");
-          print_string (Printexc.to_string exn)
+          print_string (Printexc.to_string exn ^ "\n")
     end
     ts
 
